5a7bc7441c8e838f34f826bfc3b40d99aff31e98
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-08-19 19:21:04 +0100

    boiler plate for tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,10 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+
 import { errorHandler, NotFoundError } from '@manrodri-tickets/common';
 import cookieSession from "cookie-session";
 
@@ -19,11 +16,6 @@ app.use(
 
     })
 )
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
